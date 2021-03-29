45ad91a6e319aafe08bc78c8405e3770ebdfb8e0
Ngoc Tri <mai.ngoc.tri@sun-asterisk.com>
2021-03-29 10:26:01 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import yup from './yupGlobal'
+
+const schema = yup.object().shape({
+  username: yup
+    .string()
+    .required('Required')
+    .email('Email invalid'),
+  pass: yup
+    .string()
+    .required('Required')
+    .password('Password invalid'),
+})
 
 function App() {
+  const { register, handleSubmit, errors } = useForm({
+    mode: 'onChange',
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className="box">
+      <h1>login</h1>
+      <div className="input-field">
+        <input type="text" name="username" id="username" placeholder="Email" autoComplete="off" ref={register} />
+        {errors.username && <p className="error">{errors.username.message}</p>}
+      </div>
+      <div className="input-field">
+        <input type="password" name="pass" id="pass" placeholder="Password" autoComplete="off" ref={register} />
+        {errors.pass && <p className="error">{errors.pass.message}</p>}
+      </div>
+      <button type="submit" id="submit">LOGIN</button>
+    </form>
   );
 }
 
